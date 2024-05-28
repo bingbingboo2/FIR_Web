3ac8a871c7525e46bf441ff867840879cd599fe8
diyshaikh <sh.diya6303@gmail.com>
2024-05-28 12:44:09 +0530

    ewc

--- palm_chatbot/otp.py
+++ palm_chatbot/otp.py
@@ -29,7 +29,7 @@ def otp_login():
 def generate_otp():
     otp = totp.now()
     print(f"Generated OTP: {otp}")  # Print OTP to the terminal
-    return render_template('http://174.129.169.193:8090/')
+    return render_template('\police_dashboard_project\dashboard\templates\dashboard\dashboard.html')
 
 
 
